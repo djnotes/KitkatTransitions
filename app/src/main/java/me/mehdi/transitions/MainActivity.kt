4532af01df2d59e86942e79f229ac0cd50d836ec
jasonMisaq <powergame_coder2@yahoo.com>
2018-07-15 00:22:27 +0430

    App working now by finding views again after transition and setting onClick listeners once again

--- app/src/main/java/me/mehdi/transitions/MainActivity.kt
+++ app/src/main/java/me/mehdi/transitions/MainActivity.kt
@@ -2,45 +2,80 @@ package me.mehdi.transitions
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.service.autofill.ImageTransformation
 import android.transition.*
+import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import me.mehdi.transitions.ui.main.MainFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onClick(p0: View?) {
+        when(p0?.id) {
+            R.id.register -> {
+                if (!animationStarted) {
+                    TransitionManager.go(sceneRegister, set);
+                    animationStarted = true;
+                }
+            }
+            R.id.login -> {
+                if(!animationStarted) {
+                    TransitionManager.go(sceneLogin, set);
+                    animationStarted = true;
+                }
+            }
+
+        }
+
+    }
 
 
+    var animationStarted = false;
+
+    lateinit var scene: Scene
+    lateinit var login: Button
+    lateinit var register: Button
+    lateinit var root : ViewGroup
+    lateinit var sceneRegister: Scene
+    lateinit var sceneLogin: Scene
+    lateinit var set: TransitionSet
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-//        if (savedInstanceState == null) {
-//            supportFragmentManager.beginTransaction()
-//                    .replace(R.id.container, MainFragment.newInstance())
-//                    .commitNow()
-//        }
+        login = findViewById<Button>(R.id.login);
+        register = findViewById<Button>(R.id.register);
+        root = findViewById<ViewGroup>(R.id.root);
+        scene = Scene.getSceneForLayout(root, R.layout.main_activity, this);
+        sceneLogin = Scene.getSceneForLayout(root, R.layout.main_activity_login, this);
+        sceneRegister = Scene.getSceneForLayout(root, R.layout.main_activity_register, this);
 
-        val login = findViewById<Button>(R.id.login);
-        val register = findViewById<Button>(R.id.register);
-        val root = findViewById<ViewGroup>(R.id.root);
-        val scene = Scene.getSceneForLayout(root, R.layout.main_activity, this);
-        val sceneLogin = Scene.getSceneForLayout(root, R.layout.main_activity_login, this);
-        val sceneRegister = Scene.getSceneForLayout(root, R.layout.main_activity_register, this);
-
-        val set = TransitionSet();
+        set = TransitionSet();
 
         set.addTransition(ChangeBounds()).addTransition(Fade()).addTransition(AutoTransition());
 
-        login.setOnClickListener {
-            TransitionManager.go(sceneLogin, set);
-        }
+        login.setOnClickListener (this)
+        register.setOnClickListener(this)
 
-        register.setOnClickListener {
-            TransitionManager.go(sceneRegister, set);
-        }
+
+
 
     }
 
+    override fun onBackPressed() {
+        if(animationStarted) {
+            val root = findViewById<ViewGroup>(R.id.root);
+            TransitionManager.go(scene, ChangeBounds() );
+            animationStarted = false;
+            login = findViewById(R.id.login)
+            register = findViewById(R.id.register)
+
+            login.setOnClickListener(this)
+            register.setOnClickListener(this)
+        }
+        else {
+
+            super.onBackPressed();
+        }
+
+    }
 }
